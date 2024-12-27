dec9c93364f77b98f61bf03fe937be3fe64e973e
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-12-27 15:57:29 -0500

    feat: add referral address mechanism

--- contracts/BinaryMarketingPlan.sol
+++ contracts/BinaryMarketingPlan.sol
@@ -1,4 +1,8 @@
 /**
+ *Submitted for verification at BscScan.com on 2023-03-09
+*/
+
+/**
  *Submitted for verification at BscScan.com on 2022-09-01
 */
 
@@ -226,6 +230,7 @@ contract Binary_Land is Context {
         address LeftNode;
         address RightNode;
         address UplineAddress;
+        address ReferralAddress;
         int8 DirectionOfCurrentNodeInUplineNode; // left: -1, right: 1 
         bool Status;
     }
@@ -234,28 +239,39 @@ contract Binary_Land is Context {
     mapping(address => bool) private _oldUsers;
     address[] private _usersAddresses;
 
+    mapping(uint32 => mapping(address => uint32)) private numberReferralUsers;
+
     uint256 private lastRun;
     address private owner;
     IERC20 private tetherToken;
     uint256 private registrationFee;
+    uint256 private networkFee;
+    uint256 private ownerFee;
+    uint256 private referralFee;
     uint256 private numberOfRegisteredUsersIn_24Hours;
     uint256 private totalBalance;
     uint256 private allPayments;
     uint256 private numberOfNewBalanceIn_24Hours;
     uint256 private constMaxBalanceForCalculatedReward;
+    uint32 private periodReferralRewardIndex;
     
     event UserRegistered(address indexed upLine, address indexed newUser);
 
 
     constructor(address headOfUpline, address _tetherToken) {
         owner = _msgSender();
-        registrationFee = 70 ether;
+        registrationFee = 90 ether;
+        networkFee = 45 ether;
+        ownerFee = 30 ether;
+        referralFee = 15 ether;
+
+        periodReferralRewardIndex = 0;
 
         tetherToken = IERC20(_tetherToken);
         lastRun = block.timestamp;
         numberOfRegisteredUsersIn_24Hours = 0;
         numberOfNewBalanceIn_24Hours = 0;
-        constMaxBalanceForCalculatedReward = 8  
+        constMaxBalanceForCalculatedReward = 6
         ;
         
         _users[headOfUpline] = Node({
@@ -268,6 +284,7 @@ contract Binary_Land is Context {
             LeftNode: address(0),
             RightNode: address(0),
             UplineAddress: address(0),
+            ReferralAddress: address(0),
             DirectionOfCurrentNodeInUplineNode: 1,
             Status: true
         });
@@ -342,7 +359,7 @@ contract Binary_Land is Context {
         );
     }
 
-    function A_Register(address uplineAddress) public {
+    function A_Register(address uplineAddress, address referralAddress) public {
         require(
             _users[uplineAddress].LeftNode == address(0) || _users[uplineAddress].RightNode == address(0) ,
             "This address have two directs and could not accept new members!"
@@ -354,6 +371,7 @@ contract Binary_Land is Context {
 
         require(_users[_msgSender()].Status == false, "This address is already registered!");
         require(_users[uplineAddress].Status == true, "This Upline address is Not Exist!");
+        require(_users[referralAddress].Status == true, "This referral address is Not Exist!");
 
         uint256 NumberOfCurrentBalanced;
         uint256 NumberOfNewBalanced;
@@ -367,7 +385,17 @@ contract Binary_Land is Context {
             tetherToken.safeTransferFrom(
                 _msgSender(),
                 address(this),
-                registrationFee
+                networkFee
+            );       
+            tetherToken.safeTransferFrom(
+                _msgSender(),
+                referralAddress,
+                referralFee
+            );       
+            tetherToken.safeTransferFrom(
+                _msgSender(),
+                owner,
+                ownerFee
             );       
         }
 
@@ -393,6 +421,7 @@ contract Binary_Land is Context {
             LeftNode: address(0),
             RightNode: address(0),
             UplineAddress: uplineAddress,
+            ReferralAddress: referralAddress,
             DirectionOfCurrentNodeInUplineNode: temp_DirectionOfCurrentNodeInUplineNode,
             Status: true
         });
@@ -441,7 +470,83 @@ contract Binary_Land is Context {
         }
 
         _usersAddresses.push(_msgSender());
+
+        numberReferralUsers[periodReferralRewardIndex][referralAddress] += 1;
+        
         emit UserRegistered(uplineAddress, _msgSender());
+    }
+
+    function TMM_Strategy_License() public {
+
+        require(_users[_msgSender()].Status == true, "You are not registered!");
+
+        uint256 NumberOfCurrentBalanced;
+        uint256 NumberOfNewBalanced;
+
+        address temp_UplineAddress;
+        address temp_CurrentAddress;
+        int8 temp_DirectionOfCurrentNodeInUplineNode;
+
+
+        tetherToken.safeTransferFrom(
+            _msgSender(),
+            address(this),
+            networkFee
+        );       
+        tetherToken.safeTransferFrom(
+            _msgSender(),
+            _users[_msgSender()].ReferralAddress,
+            referralFee
+        );       
+        tetherToken.safeTransferFrom(
+            _msgSender(),
+            owner,
+            ownerFee
+        ); 
+
+
+        temp_DirectionOfCurrentNodeInUplineNode = _users[_msgSender()].DirectionOfCurrentNodeInUplineNode;
+        temp_UplineAddress = _users[_msgSender()].UplineAddress;
+        temp_CurrentAddress = _msgSender();
+
+        while (true) {
+            
+            if (_users[temp_UplineAddress].Status == false) {
+                break;
+            }
+
+            if (temp_DirectionOfCurrentNodeInUplineNode == 1) {
+                _users[temp_UplineAddress].NumberOfChildNodeOnRight += 1;
+
+            } else {
+                _users[temp_UplineAddress].NumberOfChildNodeOnLeft += 1;
+
+            }
+
+            NumberOfCurrentBalanced = _users[temp_UplineAddress].NumberOfChildNodeOnLeft;
+            
+            if ( _users[temp_UplineAddress].NumberOfChildNodeOnRight < NumberOfCurrentBalanced) {
+                NumberOfCurrentBalanced = _users[temp_UplineAddress].NumberOfChildNodeOnRight;
+            }
+
+            NumberOfNewBalanced = NumberOfCurrentBalanced - (_users[temp_UplineAddress].NumberOfBalancedCalculated + _users[temp_UplineAddress].NumberOfNewBalanced);
+            
+            if (NumberOfNewBalanced > 0) {
+                _users[temp_UplineAddress].NumberOfNewBalanced += NumberOfNewBalanced;
+
+                if (_users[temp_UplineAddress].NumberOfNewBalanced <= constMaxBalanceForCalculatedReward) {
+                    totalBalance += NumberOfNewBalanced;
+                    numberOfNewBalanceIn_24Hours += NumberOfNewBalanced;
+                }
+            }
+
+            temp_CurrentAddress = temp_UplineAddress;
+            temp_DirectionOfCurrentNodeInUplineNode = _users[temp_CurrentAddress].DirectionOfCurrentNodeInUplineNode;
+            temp_UplineAddress = _users[temp_UplineAddress].UplineAddress;
+        }
+
+        numberReferralUsers[periodReferralRewardIndex][_users[_msgSender()].ReferralAddress] += 1;
+
     }
 
     function Upload_Old_User(address oldUserAddress) public onlyOwner {
@@ -452,7 +557,7 @@ contract Binary_Land is Context {
     }
 
     function Today_Contract_Balance() public view returns (uint256) {
-        return (70 ether) * numberOfRegisteredUsersIn_24Hours;
+        return networkFee * numberOfRegisteredUsersIn_24Hours;
     }
 
     function All_Time_User_Left_Right(address userAddress) public view returns(uint128 ,uint128) {
@@ -476,7 +581,7 @@ contract Binary_Land is Context {
             todayReward = 0;
 
         } else {
-            todayReward = (70 ether) * numberOfRegisteredUsersIn_24Hours / numberOfNewBalanceIn_24Hours;
+            todayReward = networkFee * numberOfRegisteredUsersIn_24Hours / numberOfNewBalanceIn_24Hours;
         }
 
         return todayReward; 
@@ -486,13 +591,6 @@ contract Binary_Land is Context {
         return _users[userAddress].RewardAmountNotReleased;
     }
     
-    function Total_User_Reward(address userAddress) public view returns(uint256) {
-        return _users[userAddress].TotalUserRewarded;
-    }
-    
-    function User_Upline(address userAddress) public view returns(address) {
-        return _users[userAddress].UplineAddress;
-    }
 
     function User_Directs_Address(address userAddress) public view returns (address, address){
         return (
@@ -509,8 +607,32 @@ contract Binary_Land is Context {
         return _users[userAddress];
     }
 
-    function Total_Number_Of_Registrations() public view returns (uint256) {
-        return _usersAddresses.length - 1;
+    function Two_Top_Referral_Users() public view returns(address, uint32, address, uint32) {
+
+        address topReferralUser1;
+        uint32 topReferralUser1_number;
+        address topReferralUser2;
+        uint32 topReferralUser2_number;
+
+        for (uint256 i=0; i < _usersAddresses.length; i = i+1) {
+            if (numberReferralUsers[periodReferralRewardIndex][_usersAddresses[i]] > 0) {
+                if (numberReferralUsers[periodReferralRewardIndex][_usersAddresses[i]] > topReferralUser1_number) {
+                    
+                    topReferralUser2 = topReferralUser1;
+                    topReferralUser2_number = topReferralUser1_number;
+                    topReferralUser1_number = numberReferralUsers[periodReferralRewardIndex][_usersAddresses[i]];
+                    topReferralUser1 = _usersAddresses[i];
+                
+                }
+                
+            }
+        }
+
+        return (topReferralUser1, topReferralUser1_number, topReferralUser2, topReferralUser2_number);
+    }
+
+    function Go_To_Next_Period_Referral_Reward() public onlyOwner {
+        periodReferralRewardIndex = periodReferralRewardIndex + 1;
     }
 
 }
